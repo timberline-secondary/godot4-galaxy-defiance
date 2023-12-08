762db83195594467542960f8d74375ebe72fdaa6
Tylere Couture <tylerecouture@gmail.com>
2023-12-07 20:09:23 -0800

    Part 8

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -5,12 +5,19 @@ extends Node2D
 @onready var scale_component = $ScaleComponent as ScaleComponent
 @onready var flash_component = $FlashComponent as FlashComponent
 @onready var shake_component = $ShakeComponent as ShakeComponent
+@onready var hurtbox_component = $HurtboxComponent as HurtboxComponent
+@onready var hitbox_component = $HitboxComponent as HitboxComponent
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
-
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		scale_component.tween_scale()
+		flash_component.flash()
+		shake_component.tween_shake()
+	)
+	stats_component.no_health.connect(queue_free)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- projectiles/laser.gd
+++ projectiles/laser.gd
@@ -2,8 +2,10 @@ extends Node2D
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 @onready var scale_component = $ScaleComponent as ScaleComponent
 @onready var flash_component = $FlashComponent as FlashComponent
+@onready var hitbox_component = $HitboxComponent as HitboxComponent
 
 func _ready():
 	scale_component.tween_scale()
 	flash_component.flash()
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hitbox_component.hit_hurtbox.connect(queue_free.unbind(1))
