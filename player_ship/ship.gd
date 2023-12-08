acb14070ead716cd5b1075e7d93c9e8846f8f5f0
Tylere Couture <tylerecouture@gmail.com>
2023-12-07 19:13:27 -0800

    Part 3

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -5,6 +5,9 @@ extends Node2D
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
 @onready var fire_rate_timer = $FireRateTimer
 @onready var scale_component:ScaleComponent = $ScaleComponent as ScaleComponent
+@onready var move_component = $MoveComponent as MoveComponent
+@onready var animated_sprite_2d = $Anchor/AnimatedSprite2D
+@onready var flame_animated_sprite = $Anchor/FlameAnimatedSprite
 
 func _ready():
 	fire_rate_timer.timeout.connect(fire_lasers)
@@ -13,3 +16,19 @@ func fire_lasers() -> void:
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
 	scale_component.tween_scale()
+	
+func _process(delta):
+	animate_the_ship()
+	
+func animate_the_ship() -> void:
+	if move_component.velocity.x < 0:
+		animated_sprite_2d.play("bank_left")
+		flame_animated_sprite.play("left")
+	elif move_component.velocity.x > 0:
+		animated_sprite_2d.play("bank_right")
+		flame_animated_sprite.play("right")
+	else:
+		animated_sprite_2d.play("center")
+		flame_animated_sprite.play("center")
+			
+	
