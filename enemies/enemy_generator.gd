8f638cac1bde012865cae5ece2fc8e6eb1e14f04
Tylere Couture <tylerecouture@gmail.com>
2023-12-21 17:06:40 -0800

    Part 16

--- enemies/enemy_generator.gd
+++ enemies/enemy_generator.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var GreenEnemyScene: PackedScene
 @export var YellowEnemyScene: PackedScene
+@export var PinkEnemyScene: PackedScene
 
 var margin = 8
 var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
@@ -9,12 +10,14 @@ var screen_width = ProjectSettings.get_setting("display/window/size/viewport_wid
 @onready var spawner_component = $SpawnerComponent as SpawnerComponent
 @onready var green_enemy_spawn_timer = $GreenEnemySpawnTimer
 @onready var yellow_enemy_spawn_timer = $YellowEnemySpawnTimer
+@onready var pink_enemy_spawn_timer = $PinkEnemySpawnTimer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawn_timer))
 	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawn_timer))
+	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene, pink_enemy_spawn_timer))
 
 
 func handle_spawn(enemy_scene: PackedScene, timer: Timer) -> void:
--- enemies/pink_enemy.gd
+++ enemies/pink_enemy.gd
@@ -5,7 +5,10 @@ extends Enemy
 @onready var move_down_state = %MoveDownState as TimedStateComponent
 @onready var move_side_state = %MoveSideState as TimedStateComponent
 @onready var pause_state = %PauseState as TimedStateComponent
-@onready var move_side_component = %MoveSideComponent
+@onready var move_side_component = %MoveSideComponent as MoveComponent
+@onready var fire_state = %FireState as StateComponent
+@onready var projectile_spawner_component = %ProjectileSpawnerComponent as SpawnerComponent
+
 
 func _ready():
 	super()
@@ -16,7 +19,14 @@ func _ready():
 	move_side_component.velocity.x = [-20,20].pick_random()
 	
 	move_down_state.state_finished.connect(move_side_state.enable)
-	move_side_state.state_finished.connect(pause_state.enable)
+	move_side_state.state_finished.connect(func():
+		fire_state.enable()
+		scale_component.tween_scale()
+		projectile_spawner_component.spawn(global_position)
+		fire_state.disable()
+		fire_state.state_finished.emit()
+	)
+	fire_state.state_finished.connect(pause_state.enable)
 	pause_state.state_finished.connect(move_down_state.enable)
 	
 	move_down_state.enable()
